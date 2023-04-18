1a24e3ef657397e77d0b07c9323b8ad73835cfa5
jbengey <72210823+jbengey@users.noreply.github.com>
2023-04-18 12:13:39 +0100

    bolt example

---
+++ Assets/Scripts/UXRGrabEvents.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UltimateXR;
+using UltimateXR.Manipulation;
+using System;
+using UnityEngine.Events;
+
+public class UXRGrabEvents : MonoBehaviour
+{
+    public UnityEvent onGrab, onRelease;
+    private UxrGrabbableObject _grab;
+
+    private void Start()
+    {
+        _grab = GetComponent<UxrGrabbableObject>();
+        _grab.Grabbed += OnGrabbed;
+    }
+
+    [ContextMenu("Editor Force onGrabbed")]
+    void testGrab()
+    {
+        OnGrabbed();
+    }
+
+
+    [ContextMenu("Editor Force onReleased")]
+    void testReleased()
+    {
+        OnReleased();
+    }
+
+    private void OnGrabbed(object sender = null, UxrManipulationEventArgs e = null)
+    {
+        onGrab?.Invoke();
+    }
+
+
+    private void OnReleased(object sender = null, UxrManipulationEventArgs e = null)
+    {
+        onRelease?.Invoke();
+    }
+
+}
